1f43387ba94aa9622e82fe30fed2da8342bfeacc
Alexander Becker <alexander.becker@tum.de>
2026-07-04 13:48:26 +0100

    Fix Windows build script version injection

--- build_win.bat
+++ build_win.bat
@@ -2,10 +2,14 @@
 
 for /f "delims=" %%i in ('git describe --tags --exact-match 2^>nul') do set "tag=%%i"
 for /f "delims=" %%i in ('git rev-parse --short HEAD 2^>nul') do set "commit=%%i"
+for /f "delims=" %%i in ('go list -m') do set "mod=%%i"
+
+set "version=%tag%"
+if "%version:~0,1%"=="v" set "version=%version:~1%"
 
 if not exist .\dist mkdir .\dist
 
 go build ^
-    -ldflags "-X main.version=%tag% -X main.commit=%commit%" ^
+    -ldflags "-X %mod%/internal/buildinfo.Version=%version% -X %mod%/internal/buildinfo.Commit=%commit%" ^
     -o .\dist\boring.exe ^
     .\cmd\boring
