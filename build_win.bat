147a11459cd05acdb929e9f1187820817142d88d
Alexander Becker <alexander.becker@tum.de>
2026-07-04 14:02:41 +0100

    Fix build_win.bat version expansion

--- build_win.bat
+++ build_win.bat
@@ -1,11 +1,12 @@
 @echo off
+setlocal enabledelayedexpansion
 
 for /f "delims=" %%i in ('git describe --tags --exact-match 2^>nul') do set "tag=%%i"
 for /f "delims=" %%i in ('git rev-parse --short HEAD 2^>nul') do set "commit=%%i"
 for /f "delims=" %%i in ('go list -m') do set "mod=%%i"
 
 set "version=%tag%"
-if "%version:~0,1%"=="v" set "version=%version:~1%"
+if defined tag if "!version:~0,1!"=="v" set "version=!version:~1!"
 
 if not exist .\dist mkdir .\dist
 
